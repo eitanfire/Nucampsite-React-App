bbb635af65071a0271ba89421bed9a26f7ed4bbc
Eitan Fire <eitanfire@gmail.com>
2022-07-21 13:41:40 -0600

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import CampsitesList from './features/campsites/CampsitesList';
-import { CAMPSITES } from './app/shared/CAMPSITES';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
 
 function App() {
     return (
         <div className='App'>
             <Header />
-            <CampsitesList />
+            <CampsitesDirectoryPage />
             <Footer />
         </div>
     );
