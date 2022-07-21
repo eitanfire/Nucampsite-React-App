9569d9a6939edc9ddf07fae648d6a1bfcbf81dd9
Eitan Fire <eitanfire@gmail.com>
2022-07-21 12:39:07 -0600

    campsitesSlice

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,15 +1,16 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-const CampsiteCard = (props) => {
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite;
     return (
         <Card>
             <CardImg 
                 width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
+                src={image}
+                alt={name}
             />
         <CardImgOverlay>
-            <CardTitle>{props.campsite.name}</CardTitle>
+            <CardTitle>{name}</CardTitle>
         </CardImgOverlay>         
         </Card>
     );
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,11 +1,13 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
+import { selectALLCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
+    const campsites = selectALLCampsites();
+
     return (
         <Row className='ms-auto'>
-            {CAMPSITES.map((campsite) => {
+            {campsites.map((campsite) => {
                 return (
                     <Col md='5' className='m-4' key={campsite.id}>
                         <CampsiteCard campsite={campsite} />
