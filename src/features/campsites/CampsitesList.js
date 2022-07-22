d11b60448dd42572d888847043144676d468802f
Eitan Fire <eitanfire@gmail.com>
2022-07-22 11:01:43 -0600

    useState

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,14 +2,19 @@ import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectALLCampsites } from './campsitesSlice';
 
-const CampsitesList = () => {
+const CampsitesList = ({ setCampsiteId }) => {
     const campsites = selectALLCampsites();
 
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
                 return (
-                    <Col md='5' className='m-4' key={campsite.id}>
+                    <Col 
+                        md='5' 
+                        className='m-4' 
+                        key={campsite.id}
+                        onClick={() => setCampsiteId(campsite.id)}
+                        >
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 );
--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,24 +1,19 @@
-import { Container, Row, Col, Button } from 'reactstrap';
+import { useState } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+import { selectCampsiteById } from '../features/campsites/campsitesSlice';
+import React from 'react';
 
 const CampsitesDirectoryPage = () => {
-    let selectedCampsite = selectRandomCampsite();
-    
-    const toggleCampsite = () => {
-        selectedCampsite = selectRandomCampsite();
-        console.log(selectedCampsite);
-    };
+    const [campsiteId, setCampsiteId] = useState(0);
+    const selectedCampsite = selectCampsiteById(campsiteId);
 
     return (
         <Container>
-            <Button onClick={() => toggleCampsite()}>
-                Select Random Campsite
-            </Button>
             <Row>
                 <Col sm='5'md='7'>
-                    <CampsitesList />
+                    <CampsitesList setCampsiteId={setCampsiteId}/>
                 </Col>
                 <Col sm='7' md='5'>
                     <CampsiteDetail campsite={selectedCampsite} />
