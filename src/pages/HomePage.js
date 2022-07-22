13ec1446b45a995dede1ddf20ea118efb93351b1
Eitan Fire <eitanfire@gmail.com>
2022-07-22 15:36:00 -0600

    Homepage Display

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,11 @@
-const HomePage = () => {
-    return ( <div>HomePage</div>
+import { Container } from 'reactstrap';
+import DisplayList from '../features/display/DisplayList';
 
+const HomePage = () => {
+    return (
+        <Container>
+            <DisplayList />
+        </Container>
     );
 };
 
