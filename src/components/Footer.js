193f98a6bda86832cda9c5e11fa4915708111dd9
Eitan Fire <eitanfire@gmail.com>
2022-07-22 12:22:18 -0600

    Header and Footer Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from 'reactstrap';
-
+import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
         <footer className='site-footer'>
@@ -7,7 +7,21 @@ const Footer = () => {
                 <Row>
 
                     <Col xs={{ size: 4, offset: 1 }} sm='2'>
-                    <h5>this will be site navigation!</h5>
+                    <h5>Links</h5>
+                    <ul className='list-unstyled'>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/directory'>Directory</Link>
+                        </li>
+                        <li>
+                            <Link to='/contact'>Contact</Link>
+                        </li>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                    </ul>
                     </Col>
 
                     <Col xs='6' sm='3' className='text-center'>
