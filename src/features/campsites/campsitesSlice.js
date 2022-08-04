4959a70c892738bc7f919ee87c150c00788080a7
Eitan Fire <eitanfire@gmail.com>
2022-08-04 10:26:12 -0600

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,16 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectALLCampsites = () => {
     return CAMPSITES;
