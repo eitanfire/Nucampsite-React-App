e5a71a18e38ea355a867aff2722b0b20f9373c12
Eitan Fire <eitanfire@gmail.com>
2022-08-05 11:59:49 -0600

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
     reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
         comments: commentsReducer,
         partners: partnersReducer,
         promotions: promotionsReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
